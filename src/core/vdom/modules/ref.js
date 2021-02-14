8dbcf7101f7f9f3617d086f97c7ea127b22e9ce1
luoxifeng <942394682@qq.com>
2021-02-14 15:55:26 +0800

    feat: refs

--- src/core/vdom/modules/ref.js
+++ src/core/vdom/modules/ref.js
@@ -3,15 +3,29 @@
 import { remove, isDef } from 'shared/util'
 
 export default {
+  /**
+   * 创建的时候注册
+   * @param {*} _ 
+   * @param {*} vnode 
+   */
   create (_: any, vnode: VNodeWithData) {
     registerRef(vnode)
   },
+  /**
+   * 更新的时候，对比新老节点的ref,如果不相等，删除旧节点的注册，注册新节点
+   * @param {*} oldVnode 
+   * @param {*} vnode 
+   */
   update (oldVnode: VNodeWithData, vnode: VNodeWithData) {
     if (oldVnode.data.ref !== vnode.data.ref) {
       registerRef(oldVnode, true)
       registerRef(vnode)
     }
   },
+  /**
+   * 销毁的时候删除
+   * @param {*} vnode 
+   */
   destroy (vnode: VNodeWithData) {
     registerRef(vnode, true)
   }
@@ -23,12 +37,19 @@ export default {
  * @param {*} isRemoval 
  */
 export function registerRef (vnode: VNodeWithData, isRemoval: ?boolean) {
+  // 如果没有ref
   const key = vnode.data.ref
   if (!isDef(key)) return
 
+  // 取上下文
   const vm = vnode.context
+  /**
+   * 注册在组件上就是组件实例，如果是原生标签就取原生标签生成的Dom
+   */
   const ref = vnode.componentInstance || vnode.elm
   const refs = vm.$refs
+
+  // 如果需要删除
   if (isRemoval) {
     if (Array.isArray(refs[key])) {
       remove(refs[key], ref)
@@ -36,6 +57,7 @@ export function registerRef (vnode: VNodeWithData, isRemoval: ?boolean) {
       refs[key] = undefined
     }
   } else {
+    // 新注册
     if (vnode.data.refInFor) {
       if (!Array.isArray(refs[key])) {
         refs[key] = [ref]
