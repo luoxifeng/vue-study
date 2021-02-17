75458e53bb69e7e8e5482fca8cd8c313cdb127e8
luoxifeng <942394682@qq.com>
2021-02-17 16:53:57 +0800

    feat: v-bind

--- src/core/instance/render-helpers/bind-object-props.js
+++ src/core/instance/render-helpers/bind-object-props.js
@@ -13,6 +13,8 @@ import {
 
 /**
  * Runtime helper for merging v-bind="object" into a VNode's data.
+ * 使用v-bind绑定的值，在内部会被处理到
+ * data或者data.attrs或者data.domProps上面
  */
 export function bindObjectProps (
   data: any,
@@ -33,6 +35,10 @@ export function bindObjectProps (
       }
       let hash
       for (const key in value) {
+        /**
+         * 如果key是class，style,以及保留属性
+         * 直接扩展到data上面
+         */
         if (
           key === 'class' ||
           key === 'style' ||
@@ -40,16 +46,29 @@ export function bindObjectProps (
         ) {
           hash = data
         } else {
+          /**
+           * 如果key值不是上述的值，则根据asProp，config.mustUseProp来决定
+           * v-bind的值扩展到哪里，
+           */
           const type = data.attrs && data.attrs.type
           hash = asProp || config.mustUseProp(tag, type, key)
             ? data.domProps || (data.domProps = {})
             : data.attrs || (data.attrs = {})
         }
+
+        /**
+         * v-bind绑定的key值，经过camelize，hyphenate后
+         * 不能与扩展目标对象上已有的属性重名
+         * 否则不会扩招到目标对象上
+         */
         const camelizedKey = camelize(key)
         const hyphenatedKey = hyphenate(key)
         if (!(camelizedKey in hash) && !(hyphenatedKey in hash)) {
           hash[key] = value[key]
 
+          /**
+           * 如果设置了同步，注册事件回调
+           */
           if (isSync) {
             const on = data.on || (data.on = {})
             on[`update:${key}`] = function ($event) {
