0e9278378e8ffbc6cb47b4d5497d274f3bd8c22a
luoxifeng <942394682@qq.com>
2021-03-13 13:24:55 +0800

    feat: class

--- src/platforms/web/util/class.js
+++ src/platforms/web/util/class.js
@@ -2,10 +2,23 @@
 
 import { isDef, isObject } from 'shared/util'
 
+/**
+ * 
+ * @param {*} vnode 
+ * 从节点上提出class合并
+ */
 export function genClassForVnode (vnode: VNodeWithData): string {
   let data = vnode.data
   let parentNode = vnode
   let childNode = vnode
+
+  /**
+   * 如果是组件占位节点，找到组件根节点，提取class合并
+   * 如果组件根节点还是一个组件，继续找这个组件的根节点，提取class合并
+   * 如果根节点还是组件节点，就按照上面的步骤找到根节点提取class合并
+   * 直到根节点不是组件节点，由此可以看出这种情况下class是会穿透的
+   * 最终会把上面层层合并好的class丢设置到根节点是普通元素的节点上
+   */
   while (isDef(childNode.componentInstance)) {
     childNode = childNode.componentInstance._vnode
     if (childNode && childNode.data) {
